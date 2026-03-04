4667da281596c37c0a13d9df642b46a469356f89
Alejo Arteaga Herrera <120742659+Alejoarteaga17@users.noreply.github.com>
2026-03-04 15:15:20 +0000

    Test python completados y correcciones full

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -1,9 +1,17 @@
-
-# TODO: Escribe pruebas unitarias para es_palindromo y suma.
-# Sugerencias:
-# - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
-# - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
-
 from python_app.palindromo import es_palindromo
 
+# Pruebas para la función es_palindromo
+def test_palindromo_radar():
+    assert es_palindromo("radar") == True
 
+def test_palindromo_anita_lava_la_tina():
+    assert es_palindromo("anita lava la tina") == True
+
+def test_palindromo_python():
+    assert es_palindromo("python") == False
+
+def test_palindromo_vacio():
+    assert es_palindromo(" ") == True
+
+def test_palindromo_radar_mayusculas():
+    assert es_palindromo("Radar") == True
--- python_app/tests/test_utils.py
+++ python_app/tests/test_utils.py
@@ -1,1 +1,10 @@
 from python_app.utils import suma
+
+def test_suma_positiva():
+    assert suma(2,3) == 5
+
+def test_suma_05():
+    assert suma(0,5) == 5
+
+def test_suma_negativo():
+    assert suma(-2,3) == 1
